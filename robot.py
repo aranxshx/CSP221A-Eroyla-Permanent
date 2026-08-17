a0c4f056161a1e8bc01f1db3c0d3def1ab0fd19c
Andrew <andreweroyla123@gmail.com>
2026-08-17 22:15:47 +0800

    Added clamped battery property with getter and setter

--- robot.py
+++ robot.py
@@ -7,10 +7,22 @@ class Robot:
         self.battery = battery
         Robot.population += 1
 
+    @property
+    def battery(self):
+        return self._battery
+
+    @battery.setter
+    def battery(self, value):
+        if value < 0:
+            self._battery = 0
+        elif value > 100:
+            self._battery = 100
+        else:
+            self._battery = value
+
 if __name__ == "__main__":
-    r1 = Robot("Bot1")
-    r2 = Robot("Bot2")
-    print(r1.name)
+    r1 = Robot("Bot1", battery=150)
+    r2 = Robot("Bot2", battery=-20)
     print(r1.battery)
+    print(r2.battery)
     print(Robot.population)
-    print(r1.manufacturer)
